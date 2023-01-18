e8de62fbe15a25f1bb1ff32cf81c6273f8c3cd1a
Sneha <snehalall.sneha@gmail.com>
2023-01-18 12:44:39 +0530

    test : add new test case

--- bowlingGameScoreFinder.test.js
+++ bowlingGameScoreFinder.test.js
@@ -6,15 +6,24 @@ describe('Bowling Game Scores', () => {
     describe('Frame 1', () => {
         it('should throw an error when the input is less than 20', () => {
 
-            expect(() => findBowlingGameScore([3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6])).toThrow('Minimum 20 inputs required');
+            expect(() => findBowlingGameScore([3, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6])).toThrow('Minimum 20 inputs required');
 
         });
 
-        it('should return the number of pins knocked down if the first frame has score less than 10', () => {
+        it('should return the score for each knocked down pin when the first frame has score less than 10', () => {
             const result = findBowlingGameScore([3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6]);
-            expect(result).toEqual(90);
+            expect(result).toBe(90);
 
         });
+
+        it('should return the score for strike when all 10 pins are striked in the first frame', () => {
+            const result = findBowlingGameScore([6, 4, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]);
+            expect(result).toBe(16);
+
+        });
+
+
+
 
 
     })
