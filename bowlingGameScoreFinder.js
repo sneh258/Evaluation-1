cf48ed5713a681e10601221b7aeae2f1b78d6cf4
Sneha <snehalall.sneha@gmail.com>
2023-01-18 12:39:34 +0530

    fix : add a new condition for scoring

--- bowlingGameScoreFinder.js
+++ bowlingGameScoreFinder.js
@@ -1,23 +1,61 @@
 const inputOne = [3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6]
 let score = 0;
 
+
+function createFrames(inputOne) {
+
+    index = 0;
+    const frames = {
+
+        'Frame 1': [inputOne[index], inputOne[index + 1]],
+        'Frame 2': [inputOne[index], inputOne[index + 1]],
+        'Frame 3': [inputOne[index], inputOne[index + 1]],
+        'Frame 4': [inputOne[index], inputOne[index + 1]],
+        'Frame 5': [inputOne[index], inputOne[index + 1]],
+        'Frame 6': [inputOne[index], inputOne[index + 1]],
+        'Frame 7': [inputOne[index], inputOne[index + 1]],
+        'Frame 8': [inputOne[index], inputOne[index + 1]],
+        'Frame 9': [inputOne[index], inputOne[index + 1]],
+        'Frame 10': [inputOne[index], inputOne[index + 1]]
+
+
+    }
+
+    return frames
+
+
+}
+
 function findBowlingGameScore(inputOne) {
     if (inputOne.length < 20) {
         throw new Error('Minimum 20 inputs required');
 
     }
 
-    for (let index = 0; index < inputOne.length; index++) {
+    for (let index = 0; index <= inputOne.length; index++) {
+
         if (inputOne[index] < 9 && inputOne[index + 1] < 9) {
-            score = inputOne[index] + inputOne[index + 1];
+            score = score + inputOne[index] + inputOne[index + 1];
+            index++;
         }
 
-        else {
-            if ((inputOne[index] + inputOne[index + 1]) == 10) {
-                score = 10 + inputOne[index + 2];
-            }
+
+        if ((inputOne[index] + inputOne[index + 1]) == 10) {
+            score = 10 + inputOne[index + 2];
+            index++;
+        }
+
+        if (inputOne[index] == 10) {
+            score = inputOne[index + 1] + inputOne[index + 2];
+            index++;
         }
 
     }
 
+    return score;
+
 }
+
+console.log(findBowlingGameScore(inputOne));
+console.log(createFrames(inputOne));
+module.exports = { findBowlingGameScore };
